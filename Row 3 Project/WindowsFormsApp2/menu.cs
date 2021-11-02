f89c66a336633b4eb910486765021e5a90d535e6
nuclearnarwhal2016 <78665377+nuclearnarwhal2016@users.noreply.github.com>
2021-11-02 08:59:33 -0400

    Update menu.cs

--- Row 3 Project/WindowsFormsApp2/menu.cs
+++ Row 3 Project/WindowsFormsApp2/menu.cs
@@ -8,5 +8,37 @@ namespace WindowsFormsApp2
 {
     class menu
     {
+        
+        
+        private int quantity;
+        private int calories;
+        private string foodType;
+        
+        public int Quantity{
+            get{return quantity;}
+            set{quantity = value;}
     }
+        public int Calories{
+        get{return calories;}
+        set{calories = value;}
+        }
+        public string FoodType{
+        get{return foodType;}
+        set{FoodType = value;}
+        }
+        
+        public menu(){
+         quantity = 0;
+            calories = 0;
+            foodType = '';
+        }
+        public int calCalories(){
+            return len(foodType)*20*quantity;
+        }
+        public void ResetDisplay(Textbox incomingTextBox){
+            incomingTextBox.Clear();   
+        }
+        public void displayValues(Textbox incomingTextBox, foodType, quantity, calories){
+               incomingTextBox.Text = Quantity.ToString() + " " + foodType + " calories:" + calories.ToString();
+        }
 }
