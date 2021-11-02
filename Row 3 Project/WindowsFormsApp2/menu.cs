64c1d1e5fafeb82ded9ddca39ecbba38289e039b
23oyehezkel <93536499+23oyehezkel@users.noreply.github.com>
2021-11-02 08:26:07 -0400

    Update menu.cs

--- Row 3 Project/WindowsFormsApp2/menu.cs
+++ Row 3 Project/WindowsFormsApp2/menu.cs
@@ -8,5 +8,12 @@ namespace WindowsFormsApp2
 {
     class menu
     {
+        int calories = 0;
+        int price = 0;
+        int hamburger = 0;
+        int hotdog = 0;
+        int fries = 0;
+        string ingredients = "beef, buns, lettuce";
+           
     }
 }
